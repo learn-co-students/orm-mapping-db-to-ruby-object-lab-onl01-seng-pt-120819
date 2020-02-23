b9647ee96b0249e999f0b5b36541436b4dd8c31c
Swishyfishie <pclandreiuk@gmail.com>
2020-02-23 01:12:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -79,6 +79,32 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY id
+    LIMIT 1
+  SQL
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+  def self.all_students_in_grade_X(arg)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ? 
+      ORDER BY students.id
+    SQL
+
+    DB[:conn].execute(sql, arg).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+
 
 
 
