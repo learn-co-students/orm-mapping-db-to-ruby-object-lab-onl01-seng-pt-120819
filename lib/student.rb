df8377b5e368b86acb29afa658865005c87b23f0
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-01 16:18:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -76,6 +76,28 @@ class Student
 
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY id
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = #{x}
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
